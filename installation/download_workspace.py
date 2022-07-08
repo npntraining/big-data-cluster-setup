a054c6572a3910e1ed86c18ed0d3d026a6f6ebca
npntraining <npn.npntraining@gmail.com>
2022-07-08 12:03:11 +0530

    Modified   pom.xml

---
+++ installation/download_workspace.py
@@ -0,0 +1,18 @@
+import gdown
+import os
+import sys
+
+BASE_PATH = "/mnt/c/Users/Naveen/"
+ARTIFACT_NAME = "big-data-masters-program-template.zip"
+
+
+def main(arg):
+    print("Downloading Big Data Masters Program Template")
+    id = arg[1]
+    gdown.download(id=id, output=BASE_PATH, quiet=False)
+    os.system("unzip {}{} -d{}".format(BASE_PATH, ARTIFACT_NAME, BASE_PATH))
+    os.system('rm {}{}'.format(BASE_PATH, ARTIFACT_NAME))
+
+
+if __name__ == '__main__':
+    main(sys.argv)
