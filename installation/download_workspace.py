e2722435eadf7abe70e2e72d368014bb3cb1d39b
npntraining <npn.npntraining@gmail.com>
2022-09-03 10:59:16 +0530

    Modified   pom.xml

--- installation/download_workspace.py
+++ installation/download_workspace.py
@@ -2,17 +2,18 @@ import gdown
 import os
 import sys
 
-BASE_PATH = "/mnt/c/Users/Naveen/"
+BASE_PATH = "/mnt/c/Users/{}/"
 ARTIFACT_NAME = "big-data-masters-program-template.zip"
 
 
 def main(arg):
     print("Downloading Big Data Masters Program Template")
     id = arg[1]
+    base_path = BASE_PATH.format(arg[2])
     gdown.download(id=id, output=BASE_PATH, quiet=False)
-    os.system("unzip {}{} -d{}".format(BASE_PATH, ARTIFACT_NAME, BASE_PATH))
-    os.system("unzip {}{}".format(BASE_PATH, ARTIFACT_NAME))
-    os.system('rm {}{}'.format(BASE_PATH, ARTIFACT_NAME))
+    os.system("unzip {}{} -d{}".format(base_path, ARTIFACT_NAME, base_path))
+    os.system("unzip {}{}".format(base_path, ARTIFACT_NAME))
+    os.system('rm {}{}'.format(base_path, ARTIFACT_NAME))
 
 
 if __name__ == '__main__':
